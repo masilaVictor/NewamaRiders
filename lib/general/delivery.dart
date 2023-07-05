80061f8f43d2e1fdc1766da6fae67c151aa83fcf
Victor Masila <masila.victor43@gmail.com>
2023-07-05 16:06:13 +0300

    new

--- lib/general/delivery.dart
+++ lib/general/delivery.dart
@@ -1,39 +1,105 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:newamariders/general/dashboard.dart';
 import 'package:newamariders/general/location_service.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
 
 class DeliveryPage extends StatefulWidget {
-  const DeliveryPage({super.key});
+  double location;
+  double location2;
+  String orderNo;
+  DeliveryPage({super.key, required this.location, required this.location2, required this.orderNo});
 
   @override
-  State<DeliveryPage> createState() => _DeliveryPageState();
+  State<DeliveryPage> createState() => _DeliveryPageState(location, location2, orderNo);
 }
 
 class _DeliveryPageState extends State<DeliveryPage> {
-  @override
-  TextEditingController _searchController = TextEditingController();
+
+
+   double location;
+  double location2;
+  String orderNo;
+  String Address = 'search';
+  final dataseRef = FirebaseDatabase.instance.ref();
+  _DeliveryPageState(this.location, this.location2, this.orderNo);
+
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+
+  Future<void> GetAddressFromLatLong(Position position)async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    Address = '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    setState(()  {
+    });
+  }
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(-1.286389, 36.817223),
+  @override
+
+
+  
+  Widget build(BuildContext context) {
+    TextEditingController _searchController = TextEditingController();
+  
+  final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(location, location2),
     zoom: 14.4746,
   );
 
-  static final Marker _kGooglePlexMarker = Marker(
+   final Marker _kGooglePlexMarker = Marker(
     markerId: MarkerId('_kGooglePlex'),
     infoWindow: InfoWindow(title: 'Google Plex'),
     icon: BitmapDescriptor.defaultMarker,
-    position: LatLng(-1.286389, 36.817223),
+    position:  LatLng(location, location2),
   );
-  static const CameraPosition _kLake = CameraPosition(
+   CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(-1.286389, 36.817223),
+      target:  LatLng(location, location2),
       tilt: 59.440717697143555,
       zoom: 30);
-  Widget build(BuildContext context) {
      return Scaffold(
       appBar: AppBar(
         title: Text('Location'),
@@ -41,35 +107,54 @@ class _DeliveryPageState extends State<DeliveryPage> {
       ),
       body: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                  child: TextFormField(
-                controller: _searchController,
-                textCapitalization: TextCapitalization.words,
-                decoration: InputDecoration(hintText: 'Search location'),
-                onChanged: (value) {
-                  print(value);
+          
+          SizedBox(
+            width: double.infinity,
+            height: 500,
+            child: Expanded(
+              child: GoogleMap(
+                mapType: MapType.normal,
+                // markers: {_kGooglePlexMarker},
+                initialCameraPosition: _kGooglePlex,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
                 },
-              )),
-              IconButton(
-                onPressed: () {
-                  LocationService().getPlace(_searchController.text);
-                },
-                icon: Icon(Icons.search),
               ),
-            ],
-          ),
-          Expanded(
-            child: GoogleMap(
-              mapType: MapType.normal,
-              // markers: {_kGooglePlexMarker},
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
             ),
           ),
+          SizedBox(
+            height: 20,
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              
+              ElevatedButton(onPressed: (){
+                showDialog(context: context, builder: (context){
+                  return AlertDialog(
+                    title: Text('Confirm Order Delivery'),
+                    content: Text('Do you want to complete order delivery?'),
+                    actions: [
+                      ElevatedButton(onPressed: (){
+                        updateOrder(
+                            'Delivered',
+                                  DateTime.now()
+                                        .millisecondsSinceEpoch
+                                        .toString());
+                        Navigator.push(context, MaterialPageRoute(builder: (context) =>const DashboardPage()));
+                      }, 
+                      child: Text('Confirm')),
+                      ElevatedButton(onPressed: (){
+                        Navigator.pop(context);
+                      }, 
+                      child: Text('Back'))
+                    ],
+                  );
+                });
+              }, child: Text('Complete'))
+            ],
+          )
         ],
       ),
       // floatingActionButton: FloatingActionButton.extended(
@@ -79,8 +164,16 @@ class _DeliveryPageState extends State<DeliveryPage> {
       // ),
     );
   }
-   Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+
+  void updateOrder(String status, var deliverTime) {
+    dataseRef
+        .child('Orders')
+        .child('${orderNo}')
+        .update({'status': status, 'DeliveryTime': deliverTime});
+    dataseRef
+        .child('Assignments')
+        .child('${orderNo}')
+        .update({'status': status, 'DeliveryTime': deliverTime});
   }
+   
 }
--- lib/general/orderview.dart
+++ lib/general/orderview.dart
@@ -2,6 +2,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:newamariders/general/delivery.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 class OrderView extends StatefulWidget {
   String orderno;
   String outlet;
@@ -18,6 +20,55 @@ class _OrderViewState extends State<OrderView> {
   String status;
   _OrderViewState(this.orderno, this.outlet, this.status);
   final dataseRef = FirebaseDatabase.instance.ref();
+
+  String location ='Null, Press Button';
+  String Address = 'search';
+  Future<Position> _getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+  Future<void> GetAddressFromLatLong(Position position)async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+    print(placemarks);
+    Placemark place = placemarks[0];
+    Address = '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    setState(()  {
+    });
+  }
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -157,9 +208,13 @@ class _OrderViewState extends State<OrderView> {
                 const SizedBox(
                   height: 20,
                 ),
-                ElevatedButton(onPressed: (){
+                ElevatedButton(onPressed: () async{
                   if(status == 'Dispatched'){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const DeliveryPage()));
+                    Position position = await _getGeoLocationPosition();
+                  double location =position.latitude;
+                  double location2 = position.longitude;
+                   GetAddressFromLatLong(position);
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => DeliveryPage(location: location, location2: location2, orderNo: orderno)));
 
                   }
                   else{
